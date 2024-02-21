711b8ee56bf72e273b76c6c2533ea8fd9a5abd6d
Stefan Dej <meteyou@gmail.com>
2024-02-21 23:58:10 +0100

    feat: add play function in MPDModule
    Signed-off-by: Stefan Dej <meteyou@gmail.com>

--- modules/MPDModule.py
+++ modules/MPDModule.py
@@ -1,4 +1,7 @@
-from mpd import MPDClient
+import logging
+
+from mpd import MPDClient, CommandError
+
 
 class MPDModule:
     def __init__(self, config):
@@ -7,6 +10,8 @@ class MPDModule:
         host = self._config.get('MPDModule', 'host', fallback='localhost')
         port = self._config.getint('MPDModule', 'port', fallback=6600)
         timeout = self._config.getint('MPDModule', 'timeout', fallback=10)
+        self._defaultVolume = self._config.getint('MPDModule', 'defaultVolume',
+                                                  fallback=100)
 
         self._client = MPDClient()
         self._client.connect(host, port)
@@ -28,3 +33,28 @@ class MPDModule:
             return {"name": name, "elapsed": elapsed, "duration": duration}
         except KeyError:
             return None
+
+    def play(self, name, elapsed=0):
+        try:
+            self._client.setvol(0)
+
+            self._client.clear()
+            self._client.add(name)
+            self._client.play()
+
+            currentSong = self._client.currentsong()
+            if elapsed + 20 > int(currentSong['time']):
+                # play from start, if trying to play too close from the end
+                elapsed = 0
+
+            logging.getLogger('sp').info('Playing %s from %s' % (name, elapsed))
+
+            self._client.seekcur(elapsed)
+            self._client.setvol(self._defaultVolume)
+
+        except CommandError as e:
+            logging.getLogger('sp').error('MPD Command error')
+            logging.getLogger('sp').exception(e)
+
+    def pause(self):
+        self._client.pause()
